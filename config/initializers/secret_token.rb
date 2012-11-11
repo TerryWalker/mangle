3e763baa2f3cb9e923e3a1d790f97945e5f34904
Teresa Walker <terry.walker2@gmail.com>
2012-11-10 22:47:16 -0800

    add a bare rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mangle::Application.config.secret_token = '08d41b9fb155a2ebc58937ec66460e1cd7d852052ff18532fb0a408e603fcfab75f83bb0272c132977f909f92e163ca33afee4e3542c1670709a62e964b6a2db'
